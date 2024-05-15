26c0f95098955736e7a9d19b9369dd66fdcff7b3
dextronox <dextronox@users.noreply.github.com>
2024-05-15 13:48:41 +0800

    Revert "Merge completed YaraEngine into main"

--- rapidobank/Security/cryptography/include/hashes/util.py
+++
@@ -1,5 +0,0 @@
-def not_32(uint_32): return 0xffffffff - uint_32
-
-def rotl_32(uint_32, shift): return ((uint_32 << shift) | (uint_32 >> (32 - shift))) & 0xffffffff
-
-def rotr_32(uint_32, shift): return ((uint_32 >> shift) | (uint_32 << (32 - shift))) & 0xFFFFFFFF
