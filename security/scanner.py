64ad31a1437a848ae7cdcd930cd70ea891299d0f
dextronox <dextronox@users.noreply.github.com>
2024-05-16 13:21:27 +0800

    Update scanner.py

--- security/scanner.py
+++ security/scanner.py
@@ -1,6 +1,6 @@
 import asyncio
 import os
-from typing import List
+from typing import Dict, List
 from yara_engine.YaraEngine_new import YaraEngine
 
 async def start(engine: YaraEngine, monitored: List[str]):
@@ -9,12 +9,13 @@ async def start(engine: YaraEngine, monitored: List[str]):
         await asyncio.sleep(5)
 
 async def scanner(engine: YaraEngine, monitored: List[str]):
-    alert = False
+    dangerous_files: Dict[str, List[str]] = {}
     for path in monitored:
         for root, dirs, files in os.walk(path):
             for file in files:
                 file_path = os.path.join(root, file)
-                if engine.scan(file_path):
-                    alert = True
-                    print(f"Alert: {file_path} matched a YARA rule.")
-    return alert
+                scan_result = engine.scan(file_path)
+                if scan_result:
+                    dangerous_files[file_path] = scan_result
+                    print(f"Alert: {file_path} matched a YARA rules {scan_result}")
+    return dangerous_files
